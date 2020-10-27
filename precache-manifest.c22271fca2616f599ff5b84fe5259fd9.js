249b9592499547dfefb44bbe3589d4ea520d9b5b
stinek4 <stinemvorner@gmail.com>
2020-10-27 22:00:33 +0100

    Updates

---
+++ precache-manifest.c22271fca2616f599ff5b84fe5259fd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec44f1f5311db4d437a23c210840607a",
+    "url": "/Laer-om-atomvaapen/index.html"
+  },
+  {
+    "revision": "728f012b4e094869adb3",
+    "url": "/Laer-om-atomvaapen/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "959fc234006624ff0220",
+    "url": "/Laer-om-atomvaapen/static/css/main.438db984.chunk.css"
+  },
+  {
+    "revision": "728f012b4e094869adb3",
+    "url": "/Laer-om-atomvaapen/static/js/2.9a9083c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Laer-om-atomvaapen/static/js/2.9a9083c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "959fc234006624ff0220",
+    "url": "/Laer-om-atomvaapen/static/js/main.e9481704.chunk.js"
+  },
+  {
+    "revision": "49661b1bc58f39351e56",
+    "url": "/Laer-om-atomvaapen/static/js/runtime-main.1481a15d.js"
+  }
+]);
